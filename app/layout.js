3ff8d85392045caac2ee714fb485899cb75a8933
AmanJ <165487083+AmanAJ21@users.noreply.github.com>
2025-04-20 18:13:43 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8069198795330862"
+     crossorigin="anonymous"></script></head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
